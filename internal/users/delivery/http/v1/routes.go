9e3bb4ea6c86ca04edee7ed9501852c2a7862c97
tkktrd <103951709+tkktrd@users.noreply.github.com>
2022-07-16 14:53:05 +0900

    fix(api/route): fix routing settings

--- internal/users/delivery/http/v1/routes.go
+++ internal/users/delivery/http/v1/routes.go
@@ -6,7 +6,6 @@ import (
 )
 
 func MapNewRoutes(v1 *gin.RouterGroup, h users.Handlers) {
-	r := v1.Group("/users")
-	r.POST("/", h.Registration())
-	r.POST("/login", h.Login())
+	v1.POST("/users", h.Registration())
+	v1.POST("/user/login", h.Login())
 }
